03888b8a3fca7d82a560ba24e4d92c253bee94d4
Islam0143 <sokoclash123@gmail.com>
2023-05-23 17:40:23 +0300

    Update time.java

--- Lab3/src/TimeAnalysis/time.java
+++ Lab3/src/TimeAnalysis/time.java
@@ -10,102 +10,123 @@ import java.util.List;
 public class time {
 
     public static void main(String[] args) {
-        ArrayList<Long> quadraticInsertTime = new ArrayList<Long>();
-        ArrayList<Long> quadraticDeleteTime = new ArrayList<Long>();
-        ArrayList<Long> quadraticSearchTime = new ArrayList<Long>();
-        ArrayList<Long> linearInsertTime = new ArrayList<Long>();
-        ArrayList<Long> linearDeleteTime = new ArrayList<Long>();
-        ArrayList<Long> linearSearchTime = new ArrayList<Long>();
-        int[] sizes = new int[]{0, 0, 0, 0, 0, 15, 40, 80, 120, 350, 500, 800, 1000, 1250, 1500, 2000, 2500, 3000};
-        for (int i = 5; i < 18; i++) {
-            String fileName = "time/file" + i;
-            QuadraticSpace<String> myHashDataStructure = new QuadraticSpace<>(sizes[i], false);
+        int[] sizes = new int[]{10, 50, 100, 250, 500, 800, 1000, 2500, 5000, 8000, 10000, 12500, 15000, 20000, 30000, 40000,
+                50000, 80000, 100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000};
+        //long[] quadraticInsertTime = new long[sizes.length];
+        //long[] quadraticDeleteTime = new long[sizes.length];
+        //long[] quadraticSearchTime = new long[sizes.length];
+        //int[] quadraticRehashCounter = new int[sizes.length];
+        long[] linearInsertTime = new long[sizes.length];
+        long[] linearDeleteTime = new long[sizes.length];
+        long[] linearSearchTime = new long[sizes.length];
+        int[] linearRehashCounter = new int[sizes.length];
+        int[] linearResizeCounter = new int[sizes.length];
+        for (int i = 16; i < 17; i++) {
+            String fileName = "time/t" + (i+1);
+            //QuadraticSpace<String> myHashDataStructure = new QuadraticSpace<>(sizes[i], false);
 
-            long start = System.nanoTime();
-            myHashDataStructure.batch_insert(fileName);
-            long end = System.nanoTime();
-            quadraticInsertTime.add(end - start);
+            long start;// = System.nanoTime();
+            //myHashDataStructure.batch_insert(fileName);
+            long end;// = System.nanoTime();
+            /*quadraticInsertTime[i] = (end - start);
+            quadraticRehashCounter[i] += myHashDataStructure.getRehashCounter();
 
             List<String> data = new Concrete_FS<String>().importData(fileName);
             start = System.nanoTime();
             for (String str : data) myHashDataStructure.search(str);
             end = System.nanoTime();
-            quadraticSearchTime.add(end - start);
+            quadraticSearchTime[i] = (end - start);
 
             start = System.nanoTime();
             myHashDataStructure.batch_delete(fileName);
             end = System.nanoTime();
-            quadraticDeleteTime.add(end - start);
+            quadraticDeleteTime[i] = (end - start);*/
 
             LinearSpace<String> myHashDataStructure2 = new LinearSpace<>(sizes[i]);
+            List<String> data = new Concrete_FS<String>().importData(fileName);
 
             start = System.nanoTime();
             myHashDataStructure2.batch_insert(fileName);
-            //System.out.println(i);///////////////////////////////
             end = System.nanoTime();
-            linearInsertTime.add(end - start);
+            linearInsertTime[i] = (end - start);
+            linearRehashCounter[i] += myHashDataStructure2.getRehashCounter();
+            linearResizeCounter[i] += myHashDataStructure2.getResizeCounter();
 
             start = System.nanoTime();
             for (String str : data) myHashDataStructure2.search(str);
             end = System.nanoTime();
-            linearSearchTime.add(end - start);
+            linearSearchTime[i] = (end - start);
 
             start = System.nanoTime();
             myHashDataStructure2.batch_delete(fileName);
             end = System.nanoTime();
-            linearDeleteTime.add(end - start);
+            linearDeleteTime[i] = (end - start);
         }
 
         for (int k = 0; k < 2; k++) {
-            for (int i = 5; i < 18; i++) {
-                String fileName = "time/file" + i;
-                QuadraticSpace<String> myHashDataStructure = new QuadraticSpace<>(sizes[i], false);
+            for (int i = 16; i < 17; i++) {
+                String fileName = "time/t" + (i+1);
+                //QuadraticSpace<String> myHashDataStructure = new QuadraticSpace<>(sizes[i], false);
 
-                long start = System.nanoTime();
-                myHashDataStructure.batch_insert(fileName);
-                long end = System.nanoTime();
-                quadraticInsertTime.set(i - 5, quadraticInsertTime.get(i - 5) + end - start);
+                long start;// = System.nanoTime();
+                //myHashDataStructure.batch_insert(fileName);
+                long end;// = System.nanoTime();
+                /*quadraticInsertTime[i] += end - start;
+                quadraticRehashCounter[i] += myHashDataStructure.getRehashCounter();
 
                 List<String> data = new Concrete_FS<String>().importData(fileName);
                 start = System.nanoTime();
                 for (String str : data) myHashDataStructure.search(str);
                 end = System.nanoTime();
-                quadraticSearchTime.set(i - 5, quadraticSearchTime.get(i - 5) + end - start);
+                quadraticSearchTime[i] += end - start;
 
                 start = System.nanoTime();
                 myHashDataStructure.batch_delete(fileName);
                 end = System.nanoTime();
-                quadraticDeleteTime.set(i - 5, quadraticDeleteTime.get(i - 5) + end - start);
+                quadraticDeleteTime[i] += end - start;*/
 
                 LinearSpace<String> myHashDataStructure2 = new LinearSpace<>(sizes[i]);
+                List<String> data = new Concrete_FS<String>().importData(fileName);
 
                 start = System.nanoTime();
                 myHashDataStructure2.batch_insert(fileName);
                 end = System.nanoTime();
-                linearInsertTime.set(i - 5, linearInsertTime.get(i - 5) + end - start);
+                linearInsertTime[i] += end - start;
+                linearRehashCounter[i] += myHashDataStructure2.getRehashCounter();
+                linearResizeCounter[i] += myHashDataStructure2.getResizeCounter();
 
                 start = System.nanoTime();
                 for (String str : data) myHashDataStructure2.search(str);
                 end = System.nanoTime();
-                linearSearchTime.set(i - 5, linearSearchTime.get(i - 5) + end - start);
+                linearSearchTime[i] += end - start;
 
                 start = System.nanoTime();
                 myHashDataStructure2.batch_delete(fileName);
                 end = System.nanoTime();
-                linearDeleteTime.set(i - 5, linearDeleteTime.get(i - 5) + end - start);
+                linearDeleteTime[i] += end - start;
             }
         }
-        for (int i = 5; i < 18; i++) {
-            quadraticInsertTime.set(i - 5, quadraticInsertTime.get(i - 5) / 3);
-            quadraticDeleteTime.set(i - 5, quadraticDeleteTime.get(i - 5) / 3);
-            quadraticSearchTime.set(i - 5, quadraticSearchTime.get(i - 5) / 3);
-            linearInsertTime.set(i - 5, quadraticInsertTime.get(i - 5) / 3);
-            linearDeleteTime.set(i - 5, quadraticDeleteTime.get(i - 5) / 3);
-            linearSearchTime.set(i - 5, quadraticSearchTime.get(i - 5) / 3);
+        for (int i = 16; i < 17; i++) {
+            //quadraticInsertTime[i] /= 3;
+            //quadraticDeleteTime[i] /= 3;
+            //quadraticSearchTime[i] /= 3;
+            linearInsertTime[i] /= 3;
+            linearDeleteTime[i] /= 3;
+            linearSearchTime[i] /= 3;
+            /*System.out.println(
+                    sizes[i] + "\t" +
+                    quadraticInsertTime[i] + "\t" + linearInsertTime[i] + "\t" +
+                    quadraticDeleteTime[i] + "\t" + linearDeleteTime[i] + "\t" +
+                    quadraticSearchTime[i] + "\t" + linearSearchTime[i] + "\t" +
+                    ((float) quadraticRehashCounter[i]) / 3 + "\t\t" + ((float) linearRehashCounter[i]) / 3 + "\t\t" +
+                    ((float) linearResizeCounter[i]) / 3 );*/
             System.out.println(
-                    quadraticInsertTime.get(i - 5) + "\t" + linearInsertTime.get(i - 5) + "\t" +
-                            quadraticDeleteTime.get(i - 5) + "\t" + linearDeleteTime.get(i - 5) + "\t" +
-                            quadraticSearchTime.get(i - 5) + "\t" + linearSearchTime.get(i - 5));
+                    sizes[i] + "\t" +
+                            linearInsertTime[i] + "\t" +
+                            linearDeleteTime[i] + "\t" +
+                            linearSearchTime[i] + "\t\t" +
+                            ((float) linearRehashCounter[i]) / 3 + "\t\t" +
+                            ((float) linearResizeCounter[i]) / 3 );
         }
 
 
